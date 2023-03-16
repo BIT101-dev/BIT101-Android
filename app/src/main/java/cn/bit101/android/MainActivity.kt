0914225b9ef509541a5475a0e1682ccf12111a91
flwfdd <1525876733@qq.com>
2023-03-16 16:37:42 +0800

    🧭建立底部导航栏框架

--- app/src/main/java/cn/bit101/android/MainActivity.kt
+++ app/src/main/java/cn/bit101/android/MainActivity.kt
@@ -3,13 +3,20 @@ package cn.bit101.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Dashboard
+import androidx.compose.material.icons.rounded.Event
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import cn.bit101.android.ui.theme.BIT101Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +24,80 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BIT101Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                MainContent()
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainContent() {
+    val navController = rememberNavController()
+
+    data class Screen(val route: String, val label: String, val icon: ImageVector)
+
+    // 底部导航栏
+    val routes = listOf(
+        Screen("schedule", "日程", Icons.Rounded.Event),
+        Screen("home", "墙", Icons.Rounded.Dashboard)
+    )
+
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+                routes.forEach { screen ->
+                    val selected= currentDestination?.hierarchy?.any { it.route == screen.route } == true
+                    NavigationBarItem(
+                        icon = { Icon(imageVector = screen.icon, contentDescription = null) },
+                        label = { Text(text = screen.label) },
+                        selected = selected,
+                        onClick = {
+                            navController.navigate(screen.route) {
+                                popUpTo(navController.graph.startDestinationId) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        })
                 }
+
+            }
+        }
+    ) {
+        NavHost(
+            navController = navController,
+            startDestination = "schedule",
+            Modifier.padding(it)
+        ) {
+            composable("schedule") {
+                Schedule()
+            }
+            composable("home") {
+                Home()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Schedule() {
+    Text(text = "Schedule")
+}
+
+@Composable
+fun Home() {
+    Text(text = "Home")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BIT101Theme {
-        Greeting("Android")
+        MainContent()
     }
 }
