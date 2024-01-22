595dd7d90bfd512d1d98adba572ea004fe5dc456
MilkFeng <3107055729@qq.com>
2024-01-22 22:23:31 +0800

    build.gradle调整

--- config/src/main/java/cn/bit101/android/config/datastore/basic/Preferences.kt
+++ config/src/main/java/cn/bit101/android/config/datastore/basic/Preferences.kt
@@ -16,6 +16,9 @@ internal class Preferences @Inject constructor(
 ) {
     private val Context.settingsDataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
+    /**
+     * 加密的 SharedPreferences，用来存储用户名、密码等敏感信息
+     */
     val ENCRYPTED_SHARED_PREFERENCES = EncryptedSharedPreferences.create(
         "encrypted_store",
         "encrypted_store_key",
@@ -23,6 +26,14 @@ internal class Preferences @Inject constructor(
         EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
         EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
     )
+
+    /**
+     * 用来存储设置项
+     */
     val SETTING_DATASTORE = context.settingsDataStore
+
+    /**
+     * 用来存储 Cookie
+     */
     val COOKIE_PREFERENCES_STORE = SharedPreferencesCookieStore(context, "cookie")
 }
