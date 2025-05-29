dad2a4ced4d9f36faba5f5f9a7a004f31fc1ae6d
NylteJ <178683943+NylteJ@users.noreply.github.com>
2025-05-29 16:07:34 +0800

    适配新版统一身份验证
    把 101 端的适配也做了, 研究了一会才发现其实很简单...
    
    顺便删除了旧的 AES 加密代码, 已经用不上了

--- data/src/main/java/cn/bit101/android/data/common/AESUtils.kt
+++ data/src/main/java/cn/bit101/android/data/common/AESUtils.kt
@@ -10,41 +10,7 @@ import javax.crypto.spec.SecretKeySpec
  * AES 加密工具类，与登录相关
  */
 internal object AESUtils {
-    private const val AES_CHARS = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678"
-
-    private fun encryptAES(data: String, key: String, iv: String): String {
-        val keyBytes = key.toByteArray(Charsets.UTF_8)
-        val ivBytes = iv.toByteArray(Charsets.UTF_8)
-        val dataBytes = data.toByteArray(Charsets.UTF_8)
-
-        val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
-        val secretKey = SecretKeySpec(keyBytes, "AES")
-        val ivSpec = IvParameterSpec(ivBytes)
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivSpec)
-        val encrypted = cipher.doFinal(dataBytes)
-        return Base64.getEncoder().encodeToString(encrypted)
-    }
-
-    private fun randomString(length: Int): String {
-        val random = SecureRandom()
-        val sb = StringBuilder(length)
-        repeat(length) {
-            val index = random.nextInt(AES_CHARS.length)
-            sb.append(AES_CHARS[index])
-        }
-        return sb.toString()
-    }
-
-    fun encryptPassword(password: String, salt: String): String {
-        return if (salt.isEmpty()) password
-        else {
-            val data = randomString(64) + password
-            encryptAES(data, salt, randomString(16))
-        }
-    }
-
-    // 适配新版的学校统一认证
-    fun encryptPasswordNew(password: String, loginCrypto: String): String {
+    fun encryptPassword(password: String, loginCrypto: String): String {
         val passwordBytes = password.toByteArray(Charsets.UTF_8)
 
         val decodedKey = Base64.getDecoder().decode(loginCrypto)
